9680a20d4bcc1505a34154a0c6e8080e3a4e73fb
Duy Khoa <duykhoa12t@gmail.com>
2016-12-15 00:27:44 +0800

    allow rating for delivery

--- spec/lib/rating_spec.rb
+++ spec/lib/rating_spec.rb
@@ -19,11 +19,25 @@ describe Rating do
     def self.find
       new
     end
+
+    def self.reset!
+      @@count = 0
+    end
+  end
+
+  after do
+    RatingModel.reset!
   end
 
   it "rate for meal" do
     rating = Rating.new(rating_model_klass: RatingModel)
     result = rating.rate(points: 4, ratable_id: 123, ratable_type: :meal)
+    expect(RatingModel.count).to eq 1
+  end
+
+  it "rate for delivery" do
+    rating = Rating.new(rating_model_klass: RatingModel)
+    result = rating.rate(points: 4, ratable_id: 125, ratable_type: :delivery)
     expect(RatingModel.count).to eq 1
   end
 end
