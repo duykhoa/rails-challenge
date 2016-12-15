bca7032095d7fce3c762c821ed77a73b86877313
Duy Khoa <duykhoa12t@gmail.com>
2016-12-15 09:57:47 +0800

    check necessary params

--- lib/rating.rb
+++ lib/rating.rb
@@ -7,11 +7,15 @@ class Rating
     point = args[:point] || 1
     ratable_type = args[:ratable_type]
     ratable_id = args[:ratable_id]
+    user_id = args[:user_id]
 
-    raise StandardError, "#ratable_id or #ratable_type is missing" unless ratable_id && ratable_type
+    raise StandardError, "#ratable_id or #ratable_type is missing" unless ratable_id && ratable_type && user_id
 
     @rating_model_klass.create(
-      ratable_id: ratable_id, point: point
+      ratable_id: ratable_id,
+      ratable_type: ratable_type,
+      user_id: user_id,
+      point: point
     )
   end
 end
--- spec/lib/rating_spec.rb
+++ spec/lib/rating_spec.rb
@@ -31,13 +31,35 @@ describe Rating do
 
   it "rate for meal" do
     rating = Rating.new(rating_model_klass: RatingModel)
-    result = rating.rate(points: 4, ratable_id: 123, ratable_type: :meal)
+    result = rating.rate(points: 4, ratable_id: 123, user_id: 6, ratable_type: :meal)
     expect(RatingModel.count).to eq 1
   end
 
   it "rate for delivery" do
     rating = Rating.new(rating_model_klass: RatingModel)
-    result = rating.rate(points: 4, ratable_id: 125, ratable_type: :delivery)
+    result = rating.rate(points: 4, ratable_id: 125, user_id: 5, ratable_type: :delivery)
     expect(RatingModel.count).to eq 1
+  end
+
+  it "lack of user_id" do
+    rating = Rating.new(rating_model_klass: RatingModel)
+    expect do
+      rating.rate(points: 4, ratable_id: 125, ratable_type: :delivery)
+    end.to raise_error StandardError
+  end
+
+  it "lack of ratable_type" do
+    rating = Rating.new(rating_model_klass: RatingModel)
+    expect do
+      rating.rate(points: 4, ratable_id: 125, user_id: 5)
+    end.to raise_error StandardError
+  end
+
+  it "lack of ratable_id" do
+    rating = Rating.new(rating_model_klass: RatingModel)
+
+    expect do
+      rating.rate(points: 4, ratable_type: :meal, user_id: 5)
+    end.to raise_error StandardError
   end
 end
