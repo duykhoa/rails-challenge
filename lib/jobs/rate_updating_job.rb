8d4f9c6d926a44e19dddd4223fbc029787d2f305
Duy Khoa <duykhoa12t@gmail.com>
2016-12-21 13:54:16 +0800

    finish logic for rate updating job

--- lib/jobs/rate_updating_job.rb
+++ lib/jobs/rate_updating_job.rb
@@ -1,2 +1,49 @@
 class RateUpdatingJob
+  PERRIOD = 1.minute.freeze
+
+  def initialize(options = {})
+    self.last_run_at = current_time - PERRIOD
+    @meal_repository = options[:meal_repository]
+    @delivery_repository = options[:delivery_repository]
+  end
+
+  def last_run_at
+    @last_run_at
+  end
+
+  def last_run_at=(time)
+    @last_run_at = time
+  end
+
+  def perform
+    run if need_to_run
+  end
+
+  def need_to_run
+    last_run_at + PERRIOD < current_time
+  end
+
+  alias_method :need_to_run?, :need_to_run
+
+  def current_time
+    Time.now
+  end
+
+  def run
+    before_run_action
+    update_meal_rate
+    update_deliver_rate
+  end
+
+  def update_meal_rate
+    @meal_repository.update_rate
+  end
+
+  def update_deliver_rate
+    @delivery_repository.update_rate
+  end
+
+  def before_run_action
+    self.last_run_at = Time.now
+  end
 end
--- spec/lib/jobs/rate_updating_job_spec.rb
+++ spec/lib/jobs/rate_updating_job_spec.rb
@@ -1,4 +1,55 @@
 require 'rails_helper'
 
 describe RateUpdatingJob do
+  it "need_to_run?" do
+    job = RateUpdatingJob.new
+    expect(job.need_to_run?).to be true
+  end
+
+  it "no need_to_run" do
+    job = RateUpdatingJob.new
+    job.last_run_at = job.current_time
+
+    expect(job.need_to_run?).to be false
+  end
+
+  class MealRepository
+    def update_rate
+      @_update_rate = true
+    end
+
+    def update_rate?
+      @_update_rate
+    end
+  end
+
+  class DeliveryRepository
+    def update_rate
+      @_update_rate = true
+    end
+
+    def update_rate?
+      @_update_rate
+    end
+  end
+
+  describe "#run" do
+    it "update_meal_rate" do
+      meal_repository = MealRepository.new
+      delivery_repository = DeliveryRepository.new
+      job =  RateUpdatingJob.new(meal_repository: meal_repository, delivery_repository: delivery_repository)
+      job.run
+
+      expect(meal_repository.update_rate?).to be true
+    end
+
+    it "update_deliver_rate" do
+      meal_repository = MealRepository.new
+      delivery_repository = DeliveryRepository.new
+      job =  RateUpdatingJob.new(meal_repository: meal_repository, delivery_repository: delivery_repository)
+      job.run
+
+      expect(delivery_repository.update_rate?).to be true
+    end
+  end
 end
