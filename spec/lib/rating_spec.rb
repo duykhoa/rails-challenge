e18cfbff7e718965ebc17aaed07000ea573bf655
Duy Khoa <duykhoa12t@gmail.com>
2016-12-22 13:32:28 +0800

    Fix specs

--- spec/lib/rating_spec.rb
+++ spec/lib/rating_spec.rb
@@ -29,37 +29,9 @@ describe Rating do
     RatingModel.reset!
   end
 
-  it "rate for meal" do
+  it "rate for order_items & delivery" do
     rating = Rating.new(rating_model_klass: RatingModel)
-    result = rating.rate(points: 4, ratable_id: 123, user_id: 6, ratable_type: :meal)
-    expect(RatingModel.count).to eq 1
-  end
-
-  it "rate for delivery" do
-    rating = Rating.new(rating_model_klass: RatingModel)
-    result = rating.rate(points: 4, ratable_id: 125, user_id: 5, ratable_type: :delivery)
-    expect(RatingModel.count).to eq 1
-  end
-
-  it "lack of user_id" do
-    rating = Rating.new(rating_model_klass: RatingModel)
-    expect do
-      rating.rate(points: 4, ratable_id: 125, ratable_type: :delivery)
-    end.to raise_error StandardError
-  end
-
-  it "lack of ratable_type" do
-    rating = Rating.new(rating_model_klass: RatingModel)
-    expect do
-      rating.rate(points: 4, ratable_id: 125, user_id: 5)
-    end.to raise_error StandardError
-  end
-
-  it "lack of ratable_id" do
-    rating = Rating.new(rating_model_klass: RatingModel)
-
-    expect do
-      rating.rate(points: 4, ratable_type: :meal, user_id: 5)
-    end.to raise_error StandardError
+    result = rating.rate(order_item: { 1 => 2, 2 => 3 }, delivery: {4 => 5})
+    expect(RatingModel.count).to eq 3
   end
 end
