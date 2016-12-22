5fd906e2504a3c372e7102aa1b30d2b42cc5bb9a
Duy Khoa <duykhoa12t@gmail.com>
2016-12-22 23:49:48 +0800

    add comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222062452) do
+ActiveRecord::Schema.define(version: 20161222144945) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.text     "content"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "comments", ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
 
   create_table "deliveries", force: :cascade do |t|
     t.integer  "ratable_id"
