631d4b23826c22eb0bf2adcf02ddd5557e85b532
Duy Khoa <duykhoa12t@gmail.com>
2016-12-22 14:10:13 +0800

    wip: add jobs

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module FeedbackApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join("lib")
+    config.autoload_paths << Rails.root.join("lib/jobs")
     config.assets.paths << "#{Rails.root}/vendor/assets/components"
   end
 end
