35041459e4e03322a10967cc6ef27b0f4a4d5814
Duy Khoa <duykhoa12t@gmail.com>
2016-12-23 16:34:30 +0800

    add doc for processor

--- lib/comment_creator.rb
+++ lib/comment_creator.rb
@@ -1,4 +1,16 @@
 class CommentCreator
+  # Public: Create comments for Delivery and OrderItem
+  #
+  #   comment_params - Hash of comments parameter
+  #     :order_item  - Hash of order_item comment, follow format ({id: content})
+  #     :delivery    - Hash of deliverycomment, follow format ({id: content})
+  #   user_id        - The Integer representation of User Id, who create comments
+  #
+  # Examples
+  #
+  #   user = User.create(email: "test@gmail.com", password: "abcdefdfd")
+  #   args = { delivery: { 1 => "this is" }, order_item: { 5 => "a comment" } }
+  #   CommentCreator.new.create(args, user.id)
   def create(comment_params, user_id)
     return unless comment_params
 
